8cf8de64e7a11b33a40b63165894085afd538140
yu.wang <wangyu@wycode.cn>
2024-12-12 18:14:17 +0800

    use hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { NextUIProvider } from '@nextui-org/react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
